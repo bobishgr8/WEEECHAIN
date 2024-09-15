271b4dd03019fb721e0981648e9bffd34b42481b
wang potao <37385782+bobishgr8@users.noreply.github.com>
2024-09-15 08:51:54 +0800

    -m nothing much has changed

--- testing.py
+++ testing.py
@@ -1,1 +1,16 @@
-print("hello world")
+import requests
+
+add = "0x7A59528aF9c81E6948AbA82eb18b4163A6485BfA"
+url = "https://testnet.veblocks.net/"
+
+def find_blocks(url):
+    response = requests.get(f"{url}/blocks/best")
+    return (response.json())
+
+def find_account(url,address):
+    response = requests.get(f"{url}/account/{address}")
+    print(response)
+    return response.json()
+
+# print(find_account(url,add))
+print(find_blocks(url))
